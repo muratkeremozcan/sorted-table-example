c0587096232ef70f62322cda63a47822fd67c103
Murat Ozcan <muratkerem@gmail.com>
2021-12-29 09:22:04 -0600

    filter big object by key & value

--- cypress/integration/1.array-vs-lodash-vs-ramda.spec.js
+++ cypress/integration/1.array-vs-lodash-vs-ramda.spec.js
@@ -1,63 +1,95 @@
 const { _, R } = Cypress
 function square(n) {
-  return n * n;
+  return n * n
 }
 
 it('array.map(fn) vs _.map(array, fn) vs R.map(fn, array)', () => {
-  // array.map(fn) 
-  cy.wrap(
-    [4, 8].map(square)
-  ).should('deep.eq', [16, 64])
-  
-  // _.map(array, fn)
-  cy.wrap(
-    _.map([4, 8], square)
-  ).should('deep.eq', [16, 64])
+  // array.map(fn)
+  cy.wrap([4, 8].map(square)).should('deep.eq', [16, 64])
 
-  cy.wrap(
-    R.map(square, [4, 8])
-  ).should('deep.eq', [16, 64])
+  // _.map(array, fn)
+  cy.wrap(_.map([4, 8], square)).should('deep.eq', [16, 64])
+
+  cy.wrap(R.map(square, [4, 8])).should('deep.eq', [16, 64])
 })
 
 it('object comparison: lodash maps over values automatically', () => {
   // no mapping over objects for array.map, so convert to array with Object.values
-  cy.wrap(
-    Object.values({ 'a': 4, 'b': 8 }).map(square)
-  ).should('deep.eq', [16, 64])
-  
+  cy.wrap(Object.values({ a: 4, b: 8 }).map(square)).should('deep.eq', [16, 64])
+
   // one advantage of lodash map is being able to map over objects
-  cy.wrap(
-    _.map({ 'a': 4, 'b': 8 }, square)
-  ).should('deep.eq', [16, 64]) 
+  cy.wrap(_.map({ a: 4, b: 8 }, square)).should('deep.eq', [16, 64])
 
   // similar to array.map situation with R.map, so convert to array with Object.values
-  cy.wrap(
-    R.map(square, Object.values({ 'a': 4, 'b': 8 }))
-  ).should('deep.eq', [16, 64]) 
-  
+  cy.wrap(R.map(square, Object.values({ a: 4, b: 8 }))).should(
+    'deep.eq',
+    [16, 64],
+  )
 })
 
 it(`KEY: the _.property iteratee shorthand is useful, esp when working with jQuery, 
 R.map(R.prop(..),arr) does a similar thing`, () => {
-  const arr = [
-    { user: 'barney' },
-    { user: 'fred' }
-  ];
+  const arr = [{ user: 'barney' }, { user: 'fred' }]
 
-  cy.wrap(
-    arr.map(i => i.user)
-  ).should('deep.eq', ['barney', 'fred'])
-   
-  cy.wrap(
-    _.map(arr, 'user')
-  ).should('deep.eq', ['barney', 'fred'])
+  cy.wrap(arr.map((i) => i.user)).should('deep.eq', ['barney', 'fred'])
+
+  cy.wrap(_.map(arr, 'user')).should('deep.eq', ['barney', 'fred'])
 
   // looks meh at first with Ramda
-  cy.wrap(
-    R.map(i => i.user, arr)
-  ).should('deep.eq', ['barney', 'fred'])
+  cy.wrap(R.map((i) => i.user, arr)).should('deep.eq', ['barney', 'fred'])
   // but you can make it better
-  cy.wrap(
-    R.map(R.prop('user'), arr)
-  ).should('deep.eq', ['barney', 'fred'])
+  cy.wrap(R.map(R.prop('user'), arr)).should('deep.eq', ['barney', 'fred'])
+})
+
+const store = {
+  accountId: 'e41de71a-8f5c-423f-945b-33c3c7fa776d',
+  address: {
+    address1: '799 Crystel Common',
+    address2: 'Apt. 790',
+    city: 'Apex',
+    country: 'New Caledonia',
+    countryCode: 'EG',
+    province: 'North Dakota',
+    provinceCode: 'OR',
+    zipCode: '84837-5370',
+  },
+  annualSales: 64642,
+  approved: true,
+  branding: {
+    primaryColor: '#000000',
+    fontUrl: 'http://tiny.cc/n3ft8y',
+    font: 'Comic Sans',
+  },
+  createdAt: 1640788366637,
+  currencyCode: 'USD',
+  domain: 'douglas.info',
+  enabled: true,
+  generateLeadsEnabled: true,
+  id: '78fd7638-a2a2-4e2c-8c9d-0946b72be3a8',
+  logoUrl: null,
+  name: 'Harvey, Zulauf and Carter',
+  phoneNumbers: [],
+  platform: 'shopify',
+  platformMetaData: {
+    platformCreatedAt: 1640788365850,
+    platformPlanName: 'ShopifyPro',
+    platformStoreName: 'Moore and Sons',
+    platformUpdatedAt: 1640788365850,
+  },
+  storeType: 'Steel',
+  updatedAt: 1640788367429,
+}
+
+it.only('big object: filter by key = value', () => {
+  // creates an object of all keys that predicate returns truthy for
+  cy.wrap(_.pickBy(store, (k) => k === true)).should('exist')
+
+  const pickEnabled = _.pick(store, 'enabled')
+  cy.wrap(_.filter(pickEnabled, (k) => k === true)).should('have.length', 1)
+
+  /**  creates an array of specified key value pair */
+  const filterByKeyValue = (key, value, obj) =>
+    _.filter(_.pick(obj, key), (v) => v === value)
+
+  cy.wrap(filterByKeyValue('enabled', true, store)).should('have.length', 1)
 })
